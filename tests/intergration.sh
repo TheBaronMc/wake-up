54a9118c7571c0535fa6ba110bdb4f19ad7d92ff
cginevra <cginevra@onestock-retail.com>
2024-11-28 19:27:02 +0100

    Add tests

--- tests/intergration.sh
+++ tests/intergration.sh
@@ -11,7 +11,7 @@ check_requirements() {
 
 start() {
     cargo run --release 2> /dev/null &
-    sleep 1
+    sleep 0.2
     pid=$!
 }
 
@@ -20,30 +20,45 @@ down() {
 }
 
 errors=0
+test_index=1
 test() {
     local test_name=$1
     local test_function=$2
+    local output_file="/tmp/wake_up_test_output_$test_index"
 
     start
 
     echo -n "$test_name : "
 
-    $test_function 2&> /dev/null
+    $test_function 2&> $output_file 
 
     if [[ $? -eq 0 ]]; then
         echo "✅"
     else
         echo "❌"
         errors=$(($errors+1))
+
+        echo "-------------------------- OUPUT ------------------------------"
+        cat $output_file
+        echo "---------------------------------------------------------------"
     fi
 
+    test_index=$(($test_index+1))
     down $pid
 }
 
 check_requirements
 
+generate_token() {
+    token=$(curl -s --location 'localhost:8999/api/login' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "password": "wake-up!"
+    }' | jq -r '.token')
+}
+
 login_wrong_pass() {
-    curl -f --location 'localhost:8999/api/login' \
+    curl -vf --location 'localhost:8999/api/login' \
     --header 'Content-Type: application/json' \
     --data '{
         "password": "toto"
@@ -57,7 +72,7 @@ login_wrong_pass() {
 }
 
 login_good_pass() {
-    curl -f --location 'localhost:8999/api/login' \
+    curl -vf --location 'localhost:8999/api/login' \
     --header 'Content-Type: application/json' \
     --data '{
         "password": "wake-up!"
@@ -66,11 +81,67 @@ login_good_pass() {
     return $?
 }
 
+configuration_reload_protected() {
+    curl -vf --location 'localhost:8999/api/configuration/reload' 
+
+    if [[ $? != 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+configuration_reload() {
+    generate_token
+
+    curl -vf --location 'localhost:8999/api/configuration/reload' \
+        -H "Authorization: Bearer $token"
+
+    return $?
+}
+
+wake_up_group_protected() {
+    curl -vf --location 'localhost:8999/api/groups/1' 
+
+    if [[ $? != 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+wake_up_host_group_protected() {
+    curl -vf --location 'localhost:8999/api/groups/1/1' 
+
+    if [[ $? != 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+wake_up_host_protected() {
+    curl -vf --location 'localhost:8999/api/hosts/1' 
+
+    if [[ $? != 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+
 echo "========================== BUILD =========================="
 cargo build --release
 echo "========================== TESTS =========================="
-test "Login with good password" login_good_pass
-test "Login with wrong password" login_wrong_pass
+test "/login good password" login_good_pass
+test "/login wrong password" login_wrong_pass
+test "/api/configuration/reload protected" configuration_reload_protected
+test "/api/configuration/reload" configuration_reload
+test "/api/groups/<group_id> protected" wake_up_group_protected
+test "/api/groups/<group_id>/<host_id> protected" wake_up_host_group_protected
+test "/api/host/<host_id> protected" wake_up_host_protected
 echo "==========================================================="
 
 exit $errors
